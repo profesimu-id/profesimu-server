f90c3bea7a87ac544691eb97ba7535609a44f8cc
suryaajis330 <suryaajis330@gmail.com>
2021-12-21 10:09:35 +0700

    init database connection

---
+++ config/db.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Setup connection to database
+func InitDatabase() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:5432/%s", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	db.AutoMigrate()
+
+	return db
+}
+
+// Stop connection to database
+func CloseDatabase(db *gorm.DB) {
+	dbPostgres, err := db.DB()
+	if err != nil {
+		panic("Failed to close connection database")
+	}
+	dbPostgres.Close()
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"response": "Home Page",
+		})
+	})
+
+	router.Run()
 }
