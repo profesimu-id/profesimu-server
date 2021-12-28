a53f7d3a262a342761bef179b7f947ef4e21f257
suryaajis330 <suryaajis330@gmail.com>
2021-12-28 17:41:55 +0700

    auth controller

--- models/user.go
+++ models/user.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
 	Name        string `gorm:"type:varchar(255)" json:"name"`
-	Email       string `gorm:"type:varchar(255)" json:"email"`
+	Email       string `gorm:"type:varchar(255),unique" json:"email"`
 	Password    string `gorm:"->;<-" json:"password"`
 	Role        string `gorm:"type:varchar(255)" json:"role"`
 	Token       string
